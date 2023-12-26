5aa3406875d96e8138857dc6b5be94ca35c2236c
KrutarthK <KrutarthK@nsdlbank.co.in>
2023-12-26 15:59:45 +0530

    code push

---
+++ app/src/main/java/com/example/composeapp/RecyclerviewActivity.kt
@@ -0,0 +1,73 @@
+package com.example.composeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeapp.ui.theme.ComposeAppTheme
+import com.example.composeapp.util.User
+import com.example.composeapp.util.dummyData
+
+class RecyclerviewActivity : ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                Recyclerview(users = dummyData())
+            }
+        }
+    }
+
+    @Composable
+    fun recyclerviewData(user: User){
+        Card(
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp).fillMaxWidth(),
+            shape = RoundedCornerShape(CornerSize(10.dp))
+        ) {
+            Row(
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.language_settings),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(80.dp)
+                        .align(Alignment.CenterVertically)
+                        .clip(
+                            RoundedCornerShape(CornerSize(10.dp))
+                        ))
+                Text(text = user.descryption, modifier = Modifier.padding(8.dp))
+            }
+        }
+    }
+
+    @Composable
+    fun Recyclerview(users : List<User>){
+        LazyColumn() {
+            items(users){userData ->
+                recyclerviewData(userData)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/composeapp/util/DataSource.kt
@@ -0,0 +1,22 @@
+package com.example.composeapp.util
+
+data class User(
+    val descryption : String
+)
+
+fun dummyData() : List<User>{
+    return listOf(
+        User("jfdhdfhfdhffhdfdhfhfhfdfdhfdhfdfhdfdhfhfdfhdfhdhfdhfdfdhfdhfdhfdhfdh"),
+        User("sksskdsksdkdskdskdskdskdskdskdsdskdskdskdkdkddkdkddkdkddkdkddkdkdkkd"),
+        User("sdmdsdkvkxlkdklkxxx,mcx,mxm,"),
+        User("kldxc,cx,.xc,.xc,x,.xc,xc,xccxcx"),
+        User("dklzdlxxccxm,cx,mcx,mcxm,xcm,xcxc,mxcm,cxm,xcm,m,cx"),
+        User("xklx;dklxcxclcx,mcx,xcm,cxm,xcm,xc,m,mxc"),
+        User("cxmlcxxcm,cxm,cx,mcxm,xccx,mcx,mcxm,xcm,m,cx"),
+        User("xc,mcx,mcxm,cx,mcxm,xcxc,mxcm,xm,cxcxm,cm,cxcx,mcxm,"),
+        User("cx,mxccxm,xcm,xccxcxm,cxcxm,cx,mxcxcm,cm,cx"),
+        User("xcm,cxm,xccxm,cxcxm,cxcxm,cx,mxc"),
+        User("cxcxmcxm,cxmcxm,cxxcm,cm,cx,mc,m,mcx"),
+        User("cxmlcxcx,mcxm,xcxcm,cx,mcxm,xcm,xcm,cx,"),
+    )
+}
