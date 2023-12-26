2c7ed935f2397ef865f740ab76d7def0403999fe
KrutarthK <KrutarthK@nsdlbank.co.in>
2023-12-26 12:08:37 +0530

    code push

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,28 +1,81 @@
 package com.example.composeapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAppTheme {
-                Surface(
+               Scaffold (
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Compose")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = {}) {
+                                    Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = {}) {
+                                    Icon(Icons.Filled.Notifications, contentDescription = "Notification")
+                                }
+                                IconButton(onClick = {}) {
+                                    Icon(Icons.Filled.Search, contentDescription = "Search")
+                                }
+                            }
+                        )
+                    },
+                    //To add floating action button
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {  })
+                        {
+                            IconButton(onClick = {}) {
+                                Icon(Icons.Filled.Add, contentDescription = "Add")
+                            }
+                        }
+                    },
+                    //floatingActionButtonPosition = FabPosition.End
+                ) {
+                    Greeting("Welcome")
+                }
+
+           /*  Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
-                }
+                }*/
             }
         }
     }
@@ -32,14 +85,16 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier.padding(70.dp),
+        color = Color.Red
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeAppTheme {
-        Greeting("Android")
+        Greeting("ggfgggggggf")
     }
 }
