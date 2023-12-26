01d71ceb456b5646e853b808cf9c079fec2b21bc
KrutarthK <KrutarthK@nsdlbank.co.in>
2023-12-26 15:06:22 +0530

    code push

---
+++ app/src/main/java/com/example/composeapp/LoginActivity.kt
@@ -0,0 +1,122 @@
+package com.example.composeapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeapp.ui.theme.ComposeAppTheme
+
+class LoginActivity : ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun LoginScreen()
+    {
+        val username = remember{ mutableStateOf("") }
+        val password = remember{ mutableStateOf("") }
+
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            verticalArrangement = Arrangement.Center
+        )
+        {
+            Text(text = "Login Screen", color = Color.Blue, fontSize = 25.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold)
+            Text(text = "Welcome", color = Color.Blue, fontSize = 25.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold)
+
+            //Todo for editText
+            OutlinedTextField (
+                value = username.value,
+                onValueChange = {
+                    username.value = it
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Person, contentDescription = "Person")
+                },
+              /*  trailingIcon = {
+                    Icon(Icons.Default.Person, contentDescription = "Person")
+                }*/
+                label = {
+                    Text(text = "Username")
+                },
+                placeholder = {
+                    Text(text = "Enter UserName")
+                },
+                modifier = Modifier.fillMaxWidth().padding(10.dp)
+            )
+
+            OutlinedTextField (
+                value = password.value,
+                onValueChange = {
+                    password.value = it
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Info, contentDescription = "Info")
+                },
+                /*  trailingIcon = {
+                      Icon(Icons.Default.Person, contentDescription = "Person")
+                  }*/
+                label = {
+                    Text(text = "Password")
+                },
+                placeholder = {
+                    Text(text = "Enter Password")
+                },
+                modifier = Modifier.fillMaxWidth().padding(10.dp)
+
+            )
+
+            OutlinedButton(onClick = {loggedIn(username.value,password.value)},modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+                Text(text = "Login")
+            }
+
+        }
+
+    }
+
+    private fun loggedIn(username : String, password : String)
+    {
+        if (username == "krutarth" && password =="1992"){
+            Toast.makeText(this,"Logged In",Toast.LENGTH_LONG).show()
+        }else{
+         Toast.makeText(this,"wrong credential",Toast.LENGTH_LONG).show()
+        }
+    }
+}
--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
@@ -19,11 +21,20 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
@@ -67,7 +78,8 @@ class MainActivity : ComponentActivity() {
                     },
                     //floatingActionButtonPosition = FabPosition.End
                 ) {
-                    Greeting("Welcome")
+                    //Greeting("Welcome")
+                   showSwitch()
                 }
 
            /*  Surface(
@@ -97,4 +109,34 @@ fun GreetingPreview() {
     ComposeAppTheme {
         Greeting("ggfgggggggf")
     }
+}
+
+//TODO Switch Logic
+@Composable
+fun showSwitch(){
+
+    //for storing the values if configuration chnages(to prevent data from loss) refer below function
+
+    val isChecked  = remember {
+        mutableStateOf(true)
+    }
+
+    Switch(
+        checked = isChecked.value,
+        onCheckedChange = {
+            isChecked.value = it
+        },
+        /*colors  = SwitchDefaults.colors(
+            checkedThumbColor = Blue,
+            checkedTrackColor = Blue,
+            uncheckedThumbColor = Yellow,
+            uncheckedTrackColor= Yellow,
+        ),*/
+        modifier = Modifier.run {
+            size(10.dp)
+            padding(100.dp)
+            //Modifier.background(if (isChecked.value) Blue else Yellow)
+        },
+
+    )
 }
